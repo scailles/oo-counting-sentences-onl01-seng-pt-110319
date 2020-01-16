1e558c16e3d64a29673f80fede57a0291912a743
Shaina C <scailles23@gmail.com>
2020-01-16 09:30:57 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -28,8 +28,10 @@ end
     end
   end
 
-  def count_sentences
-return self.count("."||"!"||"?")
+
+def count_sentences
+  new_array = self.split(/[.?!]/).reject {|string| string.empty?}
+    return new_array.length
   end
   
   
